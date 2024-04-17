ed14dba366cd941da9cacbf558b3d6e56953d31b
Justian <just.tzh@outlook.com>
2024-04-18 00:35:40 +0800

    实现win32窗口
    基于MiniEngine的最小化实现

---
+++ LearnDragonBook/GameApp.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "GameApp.h"
+
+void GameApp::Startup(void)
+{
+}
+
+void GameApp::Cleanup(void)
+{
+}
+
+void GameApp::Update(float deltaT)
+{
+}
+
+void GameApp::RenderScene(void)
+{
+}
